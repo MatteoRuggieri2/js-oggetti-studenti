b97f9ddbe36bcce1926c501d9247b52b7d1bc949
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-25 15:47:53 +0100

    Es.2 - Aggiunto array studenti, stampato nome cogn

--- js/script.js
+++ js/script.js
@@ -23,3 +23,49 @@ const student = {
 for (let key in student){
     console.log(student[key]);
 }
+
+
+
+// -- ESERCIZIO 2 --
+
+// Creo un array di oggetti con dentro gli studenti
+const studentsArray = 
+[
+
+    {
+        nome: 'Francesco',
+        cognome: 'Stecchetto',
+        eta: 34
+    },
+
+    {
+        nome: 'Guido',
+        cognome: 'Petrini',
+        eta: 22
+    },
+
+    {
+        nome: 'Giovanni',
+        cognome: 'Stanti',
+        eta: 31
+    }
+
+];
+
+
+
+// Stampo per ogni studente nome e cognome
+// leggo l'array di studenti
+for (let i = 0; i < studentsArray.length; i++) {
+    const singleStudent = studentsArray[i]
+    
+    // Per ogni studente leggo le info
+    for (let key in singleStudent) {
+        
+        // Se se la key è diversa da età stampo la info
+        if (key !== 'eta') {
+            console.log(singleStudent[key]);
+        }
+    }
+
+}
